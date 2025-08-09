16f2c78d2a6c39d24f5ae3abc44764070837c86a
segun456733 <segunsmart03@gmail.com>
2025-08-09 20:17:49 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+const navLinks = document.querySelectorAll(".nav-menu .nav-link"); // fixed selector
+const menuOpenButton = document.querySelector("#menu-open-button");
+const menuCloseButton = document.querySelector("#menu-close-button");
+
+menuOpenButton.addEventListener("click", () => {
+    document.body.classList.toggle("show-mobile-menu");
+});
+
+menuCloseButton.addEventListener("click", () => menuOpenButton.click());
+
+navLinks.forEach(link => {
+    link.addEventListener("click", () => menuOpenButton.click());
+});
+
+const swiper = new Swiper('.testimonial-swiper', {
+    loop: true,
+    spaceBetween: 25,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    breakpoints: {
+      0: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 }
+    }
+});
